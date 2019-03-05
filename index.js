b76292a2d9213a6101c7ff12088668fd2276e2c1
iampreethamveigas <veigasbros@gmail.com>
2019-03-06 01:45:23 +0530

    codes

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({ signatureVersion: 'v4' });
 
 exports.handler = (event, context, callback) => {
-  
+    let responseCode = 200;
+    const bucket = process.env.bucket || 'uploadapi1'
+    const key = event['fileName'];
+
+    try {
+        if (!bucket) {
+            callback(new Error(`bucket not set`));
+        }
+
+        if (!key) {
+            callback(new Error('FileName is missing'));
+            return;
+        }
+        const params = { 'Bucket': bucket, 'Key': key };
+
+        s3.getSignedUrl('putObject', params, (error, url) => {
+            if (error) {
+                callback(error);
+            } else {
+                let response = {
+                    statusCode: responseCode,
+                    url: url,
+                };
+                callback(null, response);
+            }
+        });
+
+    } catch (error) {
+        console.info(error)
+    }
 };
